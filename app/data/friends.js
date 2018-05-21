550f0b342ca72679b8d2f9339f2f355ae8b9f4c7
Shreedhar Amin <shreedamin@gmail.com>
2018-05-21 18:40:12 -0400

    heroku push

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+    {
+        "name": "Drake",
+        "photo": "http://www.bassboost.net/wp-content/uploads/2014/09/491956_1_drake_400.jpg",
+        "scores": [
+            1,
+            5,
+            1,
+            1,
+            5,
+            5,
+            5,
+            1,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "J.Cole",
+        "photo": "https://cdn2.gigantic.com/static/images/campaign/400x400/j_cole-4876760293.jpg",
+        "scores": [
+            1,
+            5,
+            2,
+            3,
+            3,
+            4,
+            5,
+            1,
+            5,
+            3
+        ]
+    },
+    {
+        "name": "Selena",
+        "photo": "https://s9.favim.com/orig/130820/selena-gomez-Favim.com-863751.jpg",
+        "scores": [
+            "4",
+            "5",
+            "2",
+            "1",
+            "4",
+            "5",
+            "2",
+            "3",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Ariana",
+        "photo": "https://s9.favim.com/orig/140903/america-ariana-ariana-grande-ariana-grande-butera-Favim.com-2044839.jpg",
+        "scores": [
+            1,
+            5,
+            1,
+            2,
+            2,
+            3,
+            3,
+            4,
+            5,
+            4
+        ]
+    },
+    {
+        "name": "Justin",
+        "photo": "http://www.kaseyradatti.com.au/wp-content/uploads/2015/09/JB-megamix-400x400.jpg",
+        "scores": [
+            1,
+            5,
+            5,
+            1,
+            2,
+            1,
+            4,
+            2,
+            3,
+            4
+        ]
+    },
+];
+
+module.exports = friends;
